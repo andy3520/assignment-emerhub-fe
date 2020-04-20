fe306ee95b73315f30a0e1314c70fe4ac8c13dbb
An Nguyen <an.nguyen@emerhub.com>
2020-04-20 11:53:33 +0700

    Update price

--- src/assets/value/report.js
+++ src/assets/value/report.js
@@ -1,7 +1,7 @@
 const REPORT_TYPES = [
   {
     text: "Standard Report",
-    price: 1,
+    price: 50,
     info: "Current shareholders and Board of Directors",
     value: "standard",
     standard: true
@@ -9,11 +9,11 @@ const REPORT_TYPES = [
   {
     text: "Include all revisions to the Articles of Association",
     shortTitle: "Extended Report",
-    price: 2,
+    price: 200,
     info: "All revisions to the shareholders and Board of Directors",
     value: "extended"
   }
 ]
 export {
   REPORT_TYPES
-}
+} 
